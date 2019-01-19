14daad439c7233a89209bec429ef8461dd3e79cd
Siddhant Pagariya <siddhant.pagariya@gmail.com>
2019-01-19 15:50:08 -0500

    Added Aylien Text API and testing the two APIs

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ var T = new Twitter({
     access_token: '3142021121-KyouqcIKFZFXUKIDqa60CuTqWklBW0BixEQSUfh',
     access_token_secret: 'sG2rKXJSgDBImzOV676SNhjBcqHWOJ5Kq5iF4Si9lfYLV'
 });
-var stream = T.stream('statuses/sample')
+var loc = ['-125.853', '28.74', '-66.22', '47.029'];
+var stream = T.stream('statuses/filter', { locations: loc });
+var count = 0;
 stream.on('tweet', function (tweet) {
-    console.log(tweet)
+    count++;
+    console.log(tweet);
+    if (count >= 1) {
+        stream.stop();
+    }
 })
+
+var AYLIENTextAPI = require('aylien_textapi');
+var textapi = new AYLIENTextAPI({
+    application_id: "714f8d76",
+    application_key: "5a765b0561ebf79c9b9debb7e36f087d"
+});
+textapi.sentiment({
+    'text': "Thicc"
+}, function (error, response) {
+    if (error === null) {
+        console.log(response);
+    }
+});
