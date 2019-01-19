c50620262a09a9a2c2f2215b7b60cec738f85616
Siddhant Pagariya <siddhant.pagariya@gmail.com>
2019-01-19 12:24:32 -0500

    Added Twit, replaced Twitter

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-var Twitter = require('twitter');
+var Twitter = require('twit');
 
-var client = new Twitter({
+var T = new Twitter({
     consumer_key: 'VVi6FSB9icPEDcQ394d7fJoIi',
     consumer_secret: 'qpzSCJ5jFRzXNeXCv3LqokiKr4ZXEh3BTatqG4MaiCSobPRGVk',
-    access_token_key: '3142021121-KyouqcIKFZFXUKIDqa60CuTqWklBW0BixEQSUfh',
+    access_token: '3142021121-KyouqcIKFZFXUKIDqa60CuTqWklBW0BixEQSUfh',
     access_token_secret: 'sG2rKXJSgDBImzOV676SNhjBcqHWOJ5Kq5iF4Si9lfYLV'
-  });
-  var params = {screen_name: 'SpartaHack'};
-  client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-      console.log(tweets);
-    }
-  });
+});
+var stream = T.stream('statuses/sample')
+stream.on('tweet', function (tweet) {
+    console.log(tweet)
+})
