56bbbf42401b69d6a2d5989ba86d63e1dcc12fa7
Siddhant Pagariya <siddhant.pagariya@gmail.com>
2019-01-19 22:32:50 -0500

    Added 5 Tweet polling every 2.5 seconds

--- index.js
+++ index.js
@@ -7,25 +7,67 @@ var T = new Twitter({
     access_token_secret: 'sG2rKXJSgDBImzOV676SNhjBcqHWOJ5Kq5iF4Si9lfYLV'
 });
 var loc = ['-125.853', '28.74', '-66.22', '47.029'];
-var stream = T.stream('statuses/filter', { locations: loc });
-var count = 0;
-stream.on('tweet', function (tweet) {
-    count++;
-    console.log(tweet);
-    if (count >= 1) {
-        stream.stop();
-    }
-})
 
 var AYLIENTextAPI = require('aylien_textapi');
 var textapi = new AYLIENTextAPI({
     application_id: "714f8d76",
     application_key: "5a765b0561ebf79c9b9debb7e36f087d"
 });
-textapi.sentiment({
-    'text': "Thicc"
-}, function (error, response) {
-    if (error === null) {
-        console.log(response);
+
+function handleState(state, rr) {
+    console.log(state);
+    if (rr){
+        if (rr["polarity"] === "positive"){
+            console.log(50 + 50*rr["polarity_confidence"]);
+        }
+        else if (rr["polarity"] === "negative"){
+            console.log(50 - 50*rr["polarity_confidence"]);
+        }
+        else{
+            console.log(50);
+        }
     }
-});
+}
+
+function handleSentiment(st, text) {
+    textapi.sentiment({
+        'text': text
+    }, function (error, response) {
+        if (error === null) {
+            //rr=response;//console.log(response);
+            handleState(st, response);
+        }
+    });
+}
+
+function handleTweet(tweet) {
+    var st = tweet["place"]["full_name"].split(", ")[1];
+    if (st && st.length < 3) {
+        if (tweet["extended_tweet"]) {
+            handleSentiment(st, tweet["extended_tweet"]["full_text"]);
+        } else {
+            handleSentiment(st, tweet["text"]);
+        }
+    }
+}
+
+function getTweets() {
+    var cc = 0;
+    var stream = T.stream('statuses/filter', { locations: loc });
+    stream.on('tweet', function (tweet) {
+        cc++;
+        //console.log("Tweet");
+        handleTweet(tweet);
+        if (cc >= 5) {
+            stream.stop();
+        }
+    });
+    console.log("====================");
+    setTimeout(getTweets, 2500);
+}
+getTweets();
+
+//neg pol.
+    // 50-50*(confidence)
+//pos pol. 
+    // 50 + 50(confidence)
